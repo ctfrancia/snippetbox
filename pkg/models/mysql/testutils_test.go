66021ce6ab191962232a900c13f6bc27c54ef943
Christian Francia <cfrancia@Christians-MBP.home>
2020-05-28 13:27:31 +0200

    add: integration tests for getting user

---
+++ pkg/models/mysql/testutils_test.go
@@ -0,0 +1,45 @@
+package mysql
+
+import (
+	"database/sql"
+	"io/ioutil"
+	"testing"
+)
+
+func newTestDB(t *testing.T) (*sql.DB, func()) {
+	// Establish a sql.DB connection pool for our test database. Because our
+	// setup and teardown scripts contains multiple SQL statements, we need
+	// to use the `multiStatements=true` parameter in our DSN. This instructs
+	// our MySQL database driver to support executing multiple SQL statements
+	// in one db.Exec()` call.
+	// db, err := sql.Open("mysql", "web_test:password@/test_snippet?parseTime=true&multiStatements=true")
+	db, err := sql.Open("mysql", "web_test:password@/test_snippet?parseTime=true&multiStatements=true")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	script, err := ioutil.ReadFile("./testdata/setup.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = db.Exec(string(script))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return db, func() {
+		script, err := ioutil.ReadFile("./testdata/teardown.sql")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = db.Exec(string(script))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		db.Close()
+	}
+}
